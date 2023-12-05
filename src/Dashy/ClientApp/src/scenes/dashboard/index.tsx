a1ff466b69d9635635ad60cae7af2e7d8f26a8fe
Isaiah Nelson <19826250+nonware@users.noreply.github.com>
2023-12-05 16:47:43 -0700

    Add CSSGrid for tiles and dummy dashboard

--- src/Dashy/ClientApp/src/scenes/dashboard/index.tsx
+++ src/Dashy/ClientApp/src/scenes/dashboard/index.tsx
@@ -1,9 +1,92 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, useTheme, useMediaQuery } from "@mui/material";
 type Props = {};
 
+const gridTemplateLargeScreens = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+`;
+
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+`;
+
 const Dashboard = (props: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   const { palette } = useTheme();
-  return <Box color={palette.grey[300]}>Dashboard</Box>;
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, mimmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Box bgcolor={palette.tertiary[500]} gridArea="a">
+        1x4
+      </Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="b">
+        1x4
+      </Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="c">
+        1x3
+      </Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="d"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="e"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="f"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="g"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="h"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="i"></Box>
+      <Box bgcolor={palette.tertiary[500]} gridArea="j"></Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
